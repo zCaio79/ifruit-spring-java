87b9f7c3269ab11b42c2202d97750b05d9bf3a99
zCaio79 <caiomendesfaria@gmail.com>
2024-05-10 22:01:34 -0300

    First Commit, created a `Spring Boot` project

---
+++ src/main/java/com/ifruitcommerce/ifruit/IfruitApplication.java
@@ -0,0 +1,13 @@
+package com.ifruitcommerce.ifruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifruitcommerce/ifruit/IfruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifruitcommerce.ifruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
